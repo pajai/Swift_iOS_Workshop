792b7d4a92e2c110803415298a6a9c75d64d1717
Patrick Jayet <pajai@extrabright.com>
2016-03-02 12:01:17 +0100

    Correction exercise sheet

--- Playgrounds/Exercise_3.playground/Contents.swift
+++ Playgrounds/Exercise_3.playground/Contents.swift
@@ -1,57 +1,90 @@
 /*:
 
-# Übung Wetter-App
+## Übung 3
+#Wetter-App
 
 Das Ziel von dieser Übung ist es, eine kleine Wetter-App zu programmieren.
 
 Gegeben ist ein App-Gerüst mit folgenden Komponenten/Klassen:
 
-- Ein Storyboard-File `Main.storyboard`, welches die graphische Oberfläche unserer einfachen App definiert (relativ einfach, da nur 1 Screen).
+- Ein Storyboard-File `Main.storyboard`, welches die graphische Oberfläche unserer App definiert (relativ einfach, da nur 1 Screen).
 
-- Ein Gerüst vom Hauptcontroller `ViewController`. Dieser Controller wird im Rahmen dieser Übung vervollständigt, damit unsere kleine Wetter-App korrekt funktionieren kann.
+- Ein Gerüst vom Hauptcontroller `ViewController`. Dieser Controller wird im Rahmen dieser Übung vervollständigt.
 
-- Eine Klasse `WeatherRequest`, die die Abfrage zum Server von openweathermap.org übernimmt und ein Modellobjekt mit dem Resultat der Abfrage zurückliefert.
+- Eine Klasse `WeatherRequest`, die die Abfrage zum Server von [OpenWeatherMap.org](http://openweathermap.org) übernimmt und ein Modellobjekt mit dem Resultat der Abfrage zurückliefert.
 
-- Das erwähnte Modellobjekt `WeatherData`
+- Die erwähnte Modellklasse `WeatherData`
+
+- Eine Datei `Podfile`, welche die Projektabhängigkeiten angibt. Hier haben wir einzig eine Abhängigkeit zu `SwiftyJSON` deklariert.
 
 Schaut ihr euch diese Komponenten an, um euch mit dem Projekt vertraut zu machen.
 
 
+----
 
-A) In Teil A wollen wir die UI-Elemente und Klassen vom Storyboard-File mit dem Code verbinden:
 
-- Definieren Sie im Interface-Builder die Verbindung zw. Controller-Klasse und unserer Klasse `ViewController`, damit eine Instanz von `ViewController` erzeugt wird, wenn die Storyboard-Datei geladen wird.
+A. In Teil A wollen wir das Projekt aufsetzen und die `CocoaPods` Abhängigkeiten herunterladen und einbinden.
 
-- Verbinden Sie im Interface-Builder alle Labels, welche wir nach Erhalt der Wetterinformationen vom Server ändern werden, mit deren Outlets in der Controller-Klasse `ViewController`.
+- Zuerst wollen wir das CocoaPods-Tool installieren. Dafür folgende Zeile in einem Terminal ausführen:
 
-B) Die Standortabfrage mit CoreLocation ist schon implementiert. Nun müssen Sie in der passende Lifecycle-Methode von `ViewController` den Prozess anstossen (Methode `checkPermission` aufrufen).
+    $ sudo gem install cocoapods
+    
+
+- Nun können wir das Projekt aufsetzen. Im Hauptverzeichnis vom Projekt können wir den folgenden Befehl ausführen:
+
+    $ pod install
+
+
+- Wenn man ein Projekt mit CocoaPods einrichtet, muss man jeweils das Workspace-File (hier `Swift_iOS_Workshop.xcworkspace`) und nicht das Projekt-File (mit der Endung `.xcodeproj`) aufmachen. Nun öffnen wir das erwähnte Workspace-File.
+
+
+----
+
+
+B) In Teil B wollen wir die UI-Elemente und Klassen vom Storyboard mit den entsprechenden Elementen im Code verbinden:
+
+- Im Interface-Builder stellen wir zuerst die Verbindung zw. der Controller-Klasse und unserer Klasse `ViewController` her. So wird sichergestellt, dass eine Instanz von `ViewController` erzeugt wird, wenn der Screen (so wie im Storyboard definiert) geladen wird.
+
+- Verbinden wir nun im Interface-Builder alle Labels, die wir als Outlets in der Controller-Klasse `ViewController` deklariert haben.
+
+
+----
+
+
+C) Die Standortabfrage mit CoreLocation ist schon implementiert. Wir müssen nur in der passende Lifecycle-Methode von `ViewController` den Prozess anstossen (Methode `checkPermission` aufrufen).
   
-Tipp: wir wollen `checkPermission` aufrufen, wenn der Controller erscheint.
+Tipp: wir wollen `checkPermission` aufrufen, wenn der Controller bereits erschienen ist. Um Welche Controller-Lifecycle-Methode handelt es sich hier?
 
 
-C) Nach Erhalt vom Standort wird performServerRequest aufgerufen. Lokal sollte self.location gesetzt sein. Nun wollen wir diese Methode vervollständigen.
+----
 
-- In dieser Methode wollen wir eine Instanz von WeatherRequest erzeugen und in einer Instanzvariablen von `ViewController` speichern (dies ist wichtig, damit diese Instanz nach Beendigung der Methode vom Speicher nicht geräumt wird).
+
+D) Nach Erhalt des Standortes wird die Methode `performServerRequest` aufgerufen (lokal sollte den Standort in self.location gespeichert werden). Nun wollen wir diese Methode `performServerRequest` vervollständigen.
+
+- In dieser Methode wollen wir eine Instanz von WeatherRequest erzeugen und in einer Instanzvariablen in `ViewController` speichern (dies ist wichtig, damit diese Instanz nach Beendigung der Methode vom Speicher nicht geräumt wird).
 
 - Zunächst wollen wir das Success-Block erzeugen und zuweisen. Dieser Code wird bei Erfolg der Server-Abfrage mit einer Instanz vom Modell `WeatherData` aufgerufen.
   
-Tipp: Sie können dieses Success-Block folgendermassen zuweisen:
+Tipp: Wir können dieses Success-Block folgendermassen erzeugen und zuweisen:
 
     weatherRequest?.successBlock = { weatherData in
       // etwas mit weatherData machen
     }
 
-Nun wollen wir die Daten aus weatherData lesen und die verschiedene Labels damit updaten
+Nun wollen wir die Daten aus weatherData lesen und die verschiedene Labels damit anpassen.
   
-Tipp: die Property `text` von UILabel enthält den Text vom Label, kann gelesen und geschrieben werden.
+Tipp: die Property `text` von UILabel enthält den Text vom Label (kann gelesen und geschrieben werden).
 
-- Am Ende von `performServerRequest` können wir die Methode `performRequest` von `WeatherRequest` aufrufen, damit die Server-Abfrage abgesetzt wird.
+- Am Ende von `performServerRequest` können wir die Methode `performRequest` von `WeatherRequest` aufrufen, damit die Server-Abfrage durchgeführt wird.
 
 
-D) Nun wollen wir ein Reload-Machanismus implementieren (Optional)
+----
 
-- Im Storyboard-File ist dafür ein Knopf `Neu laden` definiert. Vom Interface-Builder verbinden Sie nun dessen Aktion `Touch up inside` mit der Methode `reloadPressed` von `ViewController`.
 
-- Jetzt können Sie die Methode `reloadPressed` vervollständigen. Sie sollten zuerst `self.location` zurücksetzen (damit die CoreLocation Abfrage nochmals ausgeführt werden kann) und anschliessend `startLocationRequest` aufrufen.
+E) Nun wollen wir ein Reload-Machanismus implementieren (Optional)
+
+- Im Storyboard-File ist dafür ein Knopf `Neu laden` definiert. Vom Interface-Builder verbinden wir nun dessen Aktion `Touch up inside` mit der Methode `reloadPressed` von `ViewController`.
+
+- Jetzt können wir die Methode `reloadPressed` vervollständigen. Wir sollten zuerst `self.location` zurücksetzen (damit die CoreLocation Abfrage nochmals ausgeführt wird) und anschliessend `startLocationRequest` aufrufen.
 
 */
